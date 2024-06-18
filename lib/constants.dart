08113d54c87adbb55a71dd7bff67f150beae997c
Jeremy <l.saihong25@gmail.com>
2024-06-18 13:53:36 +0800

    change: change another tags package for simplicity

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,28 @@ final PHONE_REGEX = RegExp(r'^\d+$');
 
 final String USER_PLACEHOLDER_IMG = 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png';
 // final String USER_PLACEHOLDER_IMG = 'https://i.pravatar.cc/150?img=10';
+
+const List<String> personalityTags = [
+  'Playful',
+  'Cuddly',
+  'Independent',
+  'Intelligent',
+  'Curious',
+  'Energetic',
+  'Calm',
+  'Gentle',
+  'Affectionate',
+  'Vocal',
+  'Loyal',
+  'Mischievous',
+  'Shy',
+  'Social',
+  'Nocturnal',
+  'Crepuscular',
+  'Solitary',
+  'Drama Queen',
+  'Clown',
+  'Klutz',
+  'Grumpy',
+  'Foodie',
+];
